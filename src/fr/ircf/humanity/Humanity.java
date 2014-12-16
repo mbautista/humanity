4e3118bb09c9983e9343fc9dbe4241240fe625d5
Mathieu Bautista <mbautista@ircf.fr>
2014-12-16 13:47:16 +0100

    added .gitignore and src

---
+++ src/fr/ircf/humanity/Humanity.java
@@ -0,0 +1,17 @@
+package fr.ircf.humanity;
+
+public class Humanity {
+
+	public Humanity() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

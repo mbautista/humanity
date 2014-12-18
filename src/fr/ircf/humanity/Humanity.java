c7fe3a02b540442c7751f345c62bfc569774aa80
Mathieu Bautista <mbautista@ircf.fr>
2014-12-18 14:01:58 +0100

    implemented Slick2D window

--- src/fr/ircf/humanity/Humanity.java
+++ src/fr/ircf/humanity/Humanity.java
@@ -1,6 +1,14 @@
 package fr.ircf.humanity;
 
-public class Humanity {
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Humanity extends BasicGame {
 
 	private Title title;
 	private Menu menu;
@@ -13,17 +21,40 @@ public class Humanity {
 	private Actions actions;
 	private Audio audio;
 	private Loader loader;
+	static final String NAME = "Humanity";
+	static final double VERSION = 0.1;
 	
 	public Humanity() {
-		// TODO Auto-generated constructor stub
+		super(NAME);
 	}
 
+	@Override
+	public void init(GameContainer gc) throws SlickException {}
+
+	@Override
+	public void update(GameContainer gc, int i) throws SlickException {}
+
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException
+	{
+		g.drawString(NAME + " " + VERSION, 10, 20);
+	}
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try
+		{
+			AppGameContainer appgc;
+			appgc = new AppGameContainer(new Humanity());
+			appgc.setDisplayMode(640, 480, false);
+			appgc.start();
+		}
+		catch (SlickException ex)
+		{
+			Logger.getLogger(NAME).log(Level.SEVERE, null, ex);
+		}
 	}
 
 }

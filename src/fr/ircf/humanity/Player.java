fcc12089a1cdd5fb63ecfc82168df7f6e0ea8984
mbautista <mbautista@ircf.fr>
2014-12-26 19:14:05 +0100

    display player year, population and kardashev

--- src/fr/ircf/humanity/Player.java
+++ src/fr/ircf/humanity/Player.java
@@ -2,12 +2,14 @@ package fr.ircf.humanity;
 
 import java.util.ArrayList;
 
+import org.lwjgl.opengl.Display;
+
 public abstract class Player extends Panel implements GameElement{
 
+	private static int X = 10, DY = 20;
 	private Game game;
-	private double kardashev;
-	private double year;
-	private double population;
+	private double year = 0, population = 0, kardashev = 0;
+	private Text[] texts;
 	// TODO avatar
 	// TODO color 
 	private ArrayList<Population> populations;
@@ -15,6 +17,16 @@ public abstract class Player extends Panel implements GameElement{
 	@Override
 	public void init(Game game) throws Exception {
 		this.game = game;
+		texts = new Text[] {
+			new Text(game.i18n("player.year") + " : " + year),
+			new Text(game.i18n("player.population") + " : " + population),
+			new Text(game.i18n("player.kardashev") + " : " + kardashev),
+		};
+		int i = 0;
+		for (Text text: texts){
+			text.setPosition(X, (Display.getHeight() - getHeight()) + i * DY);
+			i++;
+		}
 	}
 
 	@Override
@@ -24,11 +36,18 @@ public abstract class Player extends Panel implements GameElement{
 
 	@Override
 	public void render() {
-		// TODO year, kardashev, population, avatar
+		// TODO avatar
+		for (Text text: texts){
+			text.render();
+		}
 	}
 
 	@Override
 	public void update(double delta) {
 		// TODO year, kardashev, population
+	}
+	
+	public int getHeight(){
+		return texts.length * (texts[0].getHeight() + DY);
 	}
 }
